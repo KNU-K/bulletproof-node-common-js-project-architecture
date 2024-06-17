a1814842946cc839fa83af369d2dc219b67c3c0a
KNU-K <knukang334@gmail.com>
2024-06-18 00:06:49 +0900

    [unit-test]간단한 TEST CODE 작성

--- src/api/controllers/user.js
+++ src/api/controllers/user.js
@@ -4,13 +4,17 @@ class UserController {
      * @param {import("express").Response}res
      * @param {import("express").NextFunction}next
      */
-    async join(req, res, next) {}
+    async join(req, res, next) {
+        return res.status(200).send('join')
+    }
     /**
      * @param {import("express").Request}req
      * @param {import("express").Response}res
      * @param {import("express").NextFunction}next
      */
-    async login(req, res, next) {}
+    async login(req, res, next) {
+        return res.status(200).send('login')
+    }
 }
 
 module.exports = UserController
