84933a6af6049a67ed31aa840015bad9df3c8c86
KNU-K <knukang334@gmail.com>
2024-06-17 23:41:28 +0900

    [initialize] config 구성 및 서버 포트 config 파일 내로 분리

--- src/app.js
+++ src/app.js
@@ -2,9 +2,7 @@ const express = require('express')
 const loader = require('./loaders')
 async function createApp() {
     const app = express()
-
     await loader.init({ expressApp: app })
-
     return app
 }
 
--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
 const createApp = require('./app')
+const { port } = require('./config')
 const { Logger } = require('./loaders/logger')
 
 async function startServer() {
     const app = await createApp()
-    const port = process.env.PORT || 8000
     app.listen(port, () => {
         Logger.info(`Server is running on port ${port}`)
     })
